3a0d1afc0e8fbcd157c6fddf4825b6f635d3605a
AllenKwok <AllenKwok@AllenKwok.local>
2016-11-03 17:29:01 +0800

    TestGit

---
+++ TestGit/TestGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGit
+//
+//  Created by AllenKwok on 2016/11/3.
+//  Copyright © 2016年 AllenKwok. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGit/TestGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGit
+//
+//  Created by AllenKwok on 2016/11/3.
+//  Copyright © 2016年 AllenKwok. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
